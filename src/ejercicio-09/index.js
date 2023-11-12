512ccda425ca48432964e51567839fc9ef14fb41
Darkrake <rachel_1723@hotmail.com>
2023-11-12 18:19:00 +0100

    Añadir ejer.9

--- src/ejercicio-09/index.js
+++ src/ejercicio-09/index.js
@@ -1,1 +1,14 @@
 // Escribe tu código aquí:
+class Person {
+    constructor(name, age) {
+      this.name = name
+      this.age = age
+    }
+  
+    introduce() {
+      console.log(`Hola, soy ${this.name} y tengo ${this.age} años.`)
+    }
+  }
+  
+  const person = new Person('Raquel', 32)
+  person.introduce()
