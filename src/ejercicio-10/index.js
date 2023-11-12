dc0f144bb19fb9a8a99f1d54345c3c47b26e3d56
Darkrake <rachel_1723@hotmail.com>
2023-11-12 18:24:11 +0100

    Añadir ejer.10

--- src/ejercicio-10/index.js
+++ src/ejercicio-10/index.js
@@ -1,1 +1,23 @@
 // Escribe tu código aquí:
+class Person {
+    constructor(name, age) {
+      this.name = name
+      this.age = age
+    }
+  
+  }
+  
+  class Student extends Person {
+    constructor(name, age, grade) {
+      super(name, age)
+      this.grade = grade
+    }
+  
+    introduce() {
+      console.log(`Hola, soy ${this.name}, tengo ${this.age} años y estoy en el grado ${this.grade}.`)
+    }
+  }
+  
+  const student = new Student('Bob', 19, 12)
+  
+  student.introduce()
